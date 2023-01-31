1d2571057673b372c36ee976bebd876712c27cfa
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-31 16:32:28 -0300

    fix: change file path read icon

--- src/helper/notificationAlert.ts
+++ src/helper/notificationAlert.ts
@@ -1,17 +1,13 @@
 import notifier from 'node-notifier'
 import path from 'node:path';
 import fs from 'node:fs';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 function notificationAlert(title: string, description?: string) {
   const mainPath = path.dirname(fs.realpathSync(__filename));
 
   notifier.notify({
     title: title,
-    message: description ? description : 'PomodoroMe',
+    message: description ? description : 'Pomodor',
     icon: path.join(mainPath, '../assets/icons/icon.jpg'),
     wait: true,
   });
