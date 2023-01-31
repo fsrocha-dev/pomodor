e0a1b77d00fc637bd1cc5358cd214d3f1da85e62
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-31 16:32:19 -0300

    fix: change file path read sound

--- src/helper/soundAlert.ts
+++ src/helper/soundAlert.ts
@@ -1,16 +1,12 @@
 import executor from 'node:child_process';
 import path from 'node:path';
 import fs from 'node:fs';
-import { fileURLToPath } from 'url';
 
 const exect = executor.exec
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const mainPath = path.dirname(fs.realpathSync(__filename));
 
 function soundAlert() {
 
+  const mainPath = path.dirname(fs.realpathSync(__filename));
   const soundPath = path.join(mainPath, '../assets/sounds/complete');
 
   const linuxcmd = 'paplay ' + soundPath + '.ogg';
